585bfa696d6b7d1dbc7692c6e13e3f86882f36f0
li <hh@123.com>
2019-07-17 19:02:35 +0800

    第一次提交

---
+++ src/main/java/com/woniu/ReraApplication.java
@@ -0,0 +1,13 @@
+package com.woniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReraApplication.class, args);
+	}
+
+}
